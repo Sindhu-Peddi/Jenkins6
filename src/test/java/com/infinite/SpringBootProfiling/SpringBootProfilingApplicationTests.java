4ca72388212a380f1b6b3d8ddec4522baaac98d0
sindhu <sindhu>
2023-11-18 22:55:10 +0530

    commit

---
+++ src/test/java/com/infinite/SpringBootProfiling/SpringBootProfilingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infinite.SpringBootProfiling;
+
+//import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootProfilingApplicationTests {
+
+	//@Test
+	void contextLoads() {
+	}
+
+}
